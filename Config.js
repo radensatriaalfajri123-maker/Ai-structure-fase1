531c2d7716eca3eb8ac8989b6a993f99c59ca679
radensatriaalfajri123-maker <radensatriaalfajri123@gmail.com>
2026-08-29 18:49:12 +0700

    Add Firebase configuration to Config.js

---
+++ Config.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA7A_WUnqgyJsmG-FH6Oo3a4w2p4rOn__s",
+    authDomain: "ai-project-fase1.firebaseapp.com",
+    databaseURL: "https://ai-project-fase1-default-rtdb.firebaseio.com",
+    projectId: "ai-project-fase1",
+    storageBucket: "ai-project-fase1.firebasestorage.app",
+    messagingSenderId: "842443540765",
+    appId: "1:842443540765:web:3d0c746d99af",
+    measurementId: "G-4MEP3EFDXY"
+};
+
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+
